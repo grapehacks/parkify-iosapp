9f28a5cf1d6032cd94d0ab328687dbb12d6c3c61
Sławomir Derwisz <slawomir.derwisz@grapeup.com>
2016-08-19 21:54:20 +0200

    Message model class

---
+++ Parkify/Parkify/Model/Message.m
@@ -0,0 +1,13 @@
+//
+//  Message.m
+//  Parkify
+//
+//  Created by Sławek Derwisz on 19/08/16.
+//  Copyright © 2016 Sławek Derwisz. All rights reserved.
+//
+
+#import "Message.h"
+
+@implementation Message
+
+@end
