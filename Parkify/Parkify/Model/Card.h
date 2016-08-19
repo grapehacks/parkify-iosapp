4c7ed4c919a99a0559e94382a5d477d87aff876a
Sławomir Derwisz <slawomir.derwisz@grapeup.com>
2016-08-19 21:10:15 +0200

    card model class

---
+++ Parkify/Parkify/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Parkify
+//
+//  Created by Sławek Derwisz on 19/08/16.
+//  Copyright © 2016 Sławek Derwisz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Parkify/Parkify/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Parkify
+//
+//  Created by Sławek Derwisz on 19/08/16.
+//  Copyright © 2016 Sławek Derwisz. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
